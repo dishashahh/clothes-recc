b0f17f22dd613441fa8840545cd366e67e0737a6
dishashahh <110770885+dishashahh@users.noreply.github.com>
2025-01-18 20:52:01 +1100

    coded clothes reccs based on temp

--- clothes_recc.py
+++ clothes_recc.py
@@ -17,13 +17,40 @@ res_json = response.json()
 hourly_data = res_json["timelines"]["hourly"]
 
 # Print hourly temperature for next 10 hours
-for i in range(10):
-    print(hourly_data[i]["values"]["temperature"])
+def temp():
+    max_temp = 0
+    min_temp = 100
+    for i in range(10):
+        hourly_temp = hourly_data[i]["values"]["temperature"]
+        if hourly_temp < min_temp:
+            min_temp = hourly_temp
+        if hourly_temp > max_temp:
+            max_temp = hourly_temp
+    # Output
+    print("Today the maximum temperature will be "+str(int(max_temp))+"°C, with a minimum of "+str(int(min_temp))+"°C.")
+    
+    if max_temp>=25:
+        print("Its a warm day today, you should wear shorts and a t-shirt.")
+    elif 18<=max_temp<25:
+        print("It will be a pleasant day - pants and a top will be your best bet.")
+    elif 12<=max_temp<18:
+        print("A chilly day today, you should wear pants and a jacket.")
+    elif max_temp<12:
+        print("BRR its a cold day, remember to layer up!")
+    
+    if min_temp<18 and max_temp>18:
+        print("But it will get cold at times, carry a light jacket.")
 
+print(temp())
+        
 # Check precipitationProbability to determine weather umbrella is required
 # if 0<PP<30 - might need an umbrella today
 # if PP>=30 - definitely will require an umbrella today!
-# TODO
+def rain_prob():
+    for i in range(10):
+        hourly_rain_prob = hourly_data[i]["values"]["precipitationProbability"]
+        print (hourly_rain_prob)
+
 
 # Check uvIndex to determine if sunscreen and a hat is required
 # if UV> 3 --> sunscreen required
